6fa6e3e4bdaecb74214808a5524c0400e21cc2f2
stephenraj-19 <stephen1331929@gmail.com>
2023-06-13 18:29:36 +0530

    java task

---
+++ ArithematicOperators.java
@@ -0,0 +1,11 @@
+public class ArithematicOperators {
+	public static void main(String args[]) {
+		int a=30;
+		int b=20;
+		System.out.println("Addition-" + (a+b));
+		System.out.println("Subtraction-" + (a-b));
+		System.out.println("Multiplication-" + (a*b));
+		System.out.println("Division-" + (a/b));
+		System.out.println("Modulus-" + (a%b));
+	}  
+}	
---
+++ UrnaryOperators.java
@@ -0,0 +1,16 @@
+public class UrnaryOperators {
+	public static void main(String args[]) {
+	int a=5;
+	System.out.println(a);	
+	System.out.println(a++);//post increment
+	System.out.println(a);
+	System.out.println(++a);//pre incerement
+	System.out.println(a);
+	System.out.println(a--);
+	System.out.println(a);
+	System.out.println(--a);
+	System.out.println(a);		
+	System.out.println(~a);//negation
+	System.out.println(!true);//inverting
+	}
+ } 
